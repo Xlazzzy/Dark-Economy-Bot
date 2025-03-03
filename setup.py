fd5ce5ae61d80e4fb284efe720d83cc61d06f225
79025 <UraykinRab@yandex.ru>
2025-03-03 14:55:53 +0300

    Add setuptools and update requirements

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+def read(f_name):
+    return open(os.path.join(os.path.dirname(__file__), f_name), encoding="utf8").read()
+
+setup(
+    name="tourist-bot",
+    version="1.0.0",
+    author_email="Egor Rassohin, Boris Boris Suhomlin, Roman Uraykin",
+    description="Events, news, and places poster",
+    long_description=read("README.md"),
+    package_data={'': ['*']},
+    install_requires=requirements,
+    include_package_data=True
+)
